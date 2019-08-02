df206723284ec9000b9988f29169b89eba0fe3a6
Nuri Akman <nuriakman@gmail.com>
2019-08-02 11:10:41 +0300

    Bootstrap başlamaya hazırız

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="tr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Blog 2019</title>
+  </head>
+  <body>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/sweetalert2.js"></script>
+  </body>
+</html>
